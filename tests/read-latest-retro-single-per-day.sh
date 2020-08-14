d3a16e420162ea4a3a8b137dbbb48ac7a9c272b3
Steve Tooke <tooky@cucumber.io>
2020-08-14 11:56:52 +0100

    Sort the files before checking latest

--- tests/read-latest-retro-single-per-day.sh
+++ tests/read-latest-retro-single-per-day.sh
@@ -1,2 +1,4 @@
-EDITOR=fake-editor retro_date=1997-06-05 retro add
+EDITOR=fake-editor retro_date=1997-06-19 retro add
+EDITOR=fake-editor retro_date=1997-06-02 retro add
+EDITOR=fake-editor retro_date=1997-04-13 retro add
 retro latest
